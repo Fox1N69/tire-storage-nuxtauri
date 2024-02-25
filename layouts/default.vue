96efe0945ca3a1cb011945e606eec61c7023fa4c
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-02-25 16:26:24 +0500

    tauri init

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="grid" style="min-height: 100vh;">
-    <LayoutSidebar/>
+    <LayoutSidebar />
     <div>
       <slot />
     </div>
@@ -12,5 +12,4 @@
   display: grid;
   grid-template-columns: 14% 6fr;
 }
-
 </style>
