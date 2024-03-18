7657aa939443e42a08fbd7f9191cce509b29b3c1
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-18 09:59:17 +0500

    cargo build

--- pages/TableRequest.vue
+++ pages/TableRequest.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
-const response = await fetch("http://127.0.0.1:8000/api/test");
-const data = await response.json();
+import type { Header, Item } from "vue3-easy-data-table";
+
+const headers: Header[] = [
+    { text: "PLAYER", value: "player" },
+    { text: "TEAM", value: "team" },
+    { text: "NUMBER", value: "number" },
+    { text: "POSITION", value: "position" },
+    { text: "HEIGHT", value: "indicator.height" },
+    { text: "WEIGHT (lbs)", value: "indicator.weight", sortable: true },
+    { text: "LAST ATTENDED", value: "lastAttended", width: 200 },
+    { text: "COUNTRY", value: "country" },
+];
+
+const items: Item[] = [
+    { player: "Stephen Curry", team: "GSW", number: 30, position: 'G', indicator: { "height": '6-2', "weight": 185 }, lastAttended: "Davidson", country: "USA" },
+    { player: "Lebron James", team: "LAL", number: 6, position: 'F', indicator: { "height": '6-9', "weight": 250 }, lastAttended: "St. Vincent-St. Mary HS (OH)", country: "USA" },
+    { player: "Kevin Durant", team: "BKN", number: 7, position: 'F', indicator: { "height": '6-10', "weight": 240 }, lastAttended: "Texas-Austin", country: "USA" },
+    { player: "Giannis Antetokounmpo", team: "MIL", number: 34, position: 'F', indicator: { "height": '6-11', "weight": 242 }, lastAttended: "Filathlitikos", country: "Greece" },
+];
 </script>
 
 <template>
     <h1 class="text-center">Hello Table Request page</h1>
     <div class="">
+        <EasyDataTable :headers="headers" :items="items" />
     </div>
 </template>
