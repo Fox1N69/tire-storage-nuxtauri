0da9dce72f2239b22e37879c784274894e9149cc
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-20 14:53:06 +0500

    install axios but he was must work

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  typescript: {
+    strict: true
+  },
   devtools: { enabled: true },
   modules: [
-    '@nuxt/image',
+    "@nuxt/image",
     "@nuxtjs/tailwindcss",
-    'shadcn-nuxt',
+    "shadcn-nuxt",
     "nuxt-icon",
-    '@pinia/nuxt',
+    "@pinia/nuxt",
+    "@nuxtjs/axios",
   ],
+  axios: {
+    // extra config e.g
+    BaseURL: 'https://127.0.0.1:4000'
+  },
   shadcn: {
     /**
      * Prefix for all the imported component
      */
-    prefix: 'Ui',
+    prefix: "Ui",
     /**
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: './components/ui'
+    componentDir: "./components/ui",
   },
-  pinia:{
-    storesDir: ["./store/**"],
-  }
-})
+  
+});
