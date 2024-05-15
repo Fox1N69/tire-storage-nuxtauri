53d324d842917566b440f581389806a090012b69
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-05-15 14:15:46 +0500

    add data-table-vue

---
+++ plugins/easy-data-table.ts
@@ -0,0 +1,7 @@
+import { defineNuxtPlugin } from "nuxt/app";
+import Vue3EasyDataTable from "vue3-easy-data-table";
+import "vue3-easy-data-table/dist/style.css";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.component("EasyDataTable", Vue3EasyDataTable);
+});
